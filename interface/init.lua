514e3f56cb12aa757cb484d1e504285e62b415a1
delgyd <delgyd@163.com>
2017-12-07 21:25:09 +0800

    更换redis二次封装
    使用Anjia的redis二次封装，解决key操作问题

--- interface/init.lua
+++ interface/init.lua
@@ -3,15 +3,29 @@ local modulename = "init"
 local _M = {}
 
 _M._VERSION = '0.0.1'
+-- _M.Redis = {
+--         ["host"]        = ngx.var.redis_host,
+--         ["port"]        = ngx.var.redis_port,
+--         ["poolsize"]    = ngx.var.redis_pool_size,
+--         ["idletime"]    = ngx.var.redis_keepalive_timeout,
+--         ["timeout"]     = ngx.var.redis_connect_timeout,
+--         ["dbid"]        = ngx.var.redis_dbid,
+--         ["pwd"]         = ngx.var.redis_pwd,
+--         ['db_index']	= 0,
+--         ['password']	= ngx.var.redis_pwd,
+--         ['keepalive']	= ngx.var.redis_keepalive_timeout,
+--         ['pool_size']	= ngx.var.redis_pool_size,
+-- }
+
 _M.Redis = {
-        ["host"]        = ngx.var.redis_host,
-        ["port"]        = ngx.var.redis_port,
-        ["poolsize"]    = ngx.var.redis_pool_size,
-        ["idletime"]    = ngx.var.redis_keepalive_timeout,
-        ["timeout"]     = ngx.var.redis_connect_timeout,
-        ["dbid"]        = ngx.var.redis_dbid,
-        ["pwd"]         = ngx.var.redis_pwd,
-}
+                ['host']= '192.168.1.2',
+                ['port']=6388,
+                ['db_index']=0,
+                ['password']='qwert',
+                ['timeout']=1000,
+                ['keepalive']=60000,
+                ['pool_size']=100,
+            }
 _M.Service = {
         ["service"]    = ngx.var.server,
 }
