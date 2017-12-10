084a557e2eeb0d3470978223eef15475c3e67452
delgyd <delgyd@163.com>
2017-12-10 15:42:47 +0800

    nginx配置lua脚本入口

---
+++ index.lua
@@ -0,0 +1,125 @@
+----lua_index.lua
+--1、判断限流，为常开
+--2、判断平滑，为可选
+local rediscmd = require('interface.utils.rediscmd')
+local limit_mode = require('interface.limit.limitindex')
+local upgrade = require('interface.upgrade.upindex')
+local socket = require("socket")
+
+local function writefile(filename, info)
+    local wfile=io.open(filename, "a+")
+    assert(wfile)
+    wfile:write(info,'\n')
+    wfile:close()
+end
+local limitlog = '/data/webapp/logs/limit_log/lua-'..os.date("%Y")..os.date("%m")..os.date("%d")..os.date("%H")..'.log'
+local limitinfo = '['..os.date()..']'
+
+local url = ngx.var.uri
+if url == '/scmccClient/servlet/chargeNotify.do' then --充值回调
+	return
+end
+--[[
+-影响执行性能
+--获取本机IP
+local function GetAdd(hostname)
+	local ip, resolved = socket.dns.toip(hostname)
+	local ListTab = {}
+	for k, v in ipairs(resolved.ip) do
+		table.insert(ListTab, v)
+	end
+	return ListTab
+end
+	--获取本服务器IP
+local hostip = unpack(GetAdd(socket.dns.gethostname()))
+--]]
+
+--获取当前连接数
+local active = tonumber(ngx.var.connections_active)
+if not active then
+    active = 0
+end
+--根据缓存可用性选用随机精确值
+local ok,err = rediscmd:ping()
+if ok == 'PONG' then
+	--获取请求随机数毫秒级，可用于防刷操作
+	math.randomseed(tostring(socket.gettime()):reverse():sub(1, 10)) 
+	rdm = math.random(1,100)
+else
+	--获取请求随机数秒级
+	math.randomseed(tostring(os.time()):reverse():sub(1, 10))
+	rdm = math.random(1,100)
+end
+
+--获取用户ID
+local ckv = ngx.var["cookie_SmsNoPwdLoginCookie"]
+if not ckv then return end
+
+local grade1,err = rediscmd:hget('limit','grade1')
+local grade2,err = rediscmd:hget('limit','grade2')
+local grade3,err = rediscmd:hget('limit','grade3')
+
+local info_limit = {
+	['id'] = ckv,
+	['status'] = 'limit',
+	['active'] = active,
+	['url'] = url,
+	['grade1'] = tonumber(grade1) or 30,
+	['grade2'] = tonumber(grade1) or 60,
+	['grade3'] = tonumber(grade1) or 90,
+	['rdm'] = rdm,
+	['hostip'] = ngx.var.serverip,
+}
+local info_pass = {
+	['id'] = ckv,
+	['status'] = 'pass',
+	['active'] = active,
+	['url'] = url,
+	['grade1'] = tonumber(grade1) or 30,
+	['grade2'] = tonumber(grade1) or 60,
+	['grade3'] = tonumber(grade1) or 90,
+	['rdm'] = rdm,
+	['hostip'] = ngx.var.serverip,
+}
+
+--获取黑白名单；黑名单请求限制访问，白名单请求，只直接跳过,黑名单将info_pass状态重置为limit
+
+local whiteid,err = rediscmd:sismember('limit_white_list',ckv)
+local whiteurl,err = rediscmd:sismember('limit_white_list',url)
+if whiteid == 1 or whiteurl == 1 then
+	writefile(limitlog,limitinfo..'|'..url..'|'..active..'|'..ckv..'|'..'pass'..'白名单请求')
+	return
+end
+local blackid,err = rediscmd:sismember('limit_black_list',ckv)
+local blackurl,err = rediscmd:sismember('limit_black_list',url)
+if blackid == 1 or blackurl == 1 then
+	info_pass['status'] = 'limit'
+	writefile(limitlog,limitinfo..'|'..url..'|'..active..'|'..ckv..'|'..'limit'..'黑名单请求')
+end
+
+--根据url匹配业务模块scmccClient/scmccClientWap/scmccCampaign/xxxxx
+if url == "/scmccClient/action.dox" or url == "/scmccClient/action.dox?" then
+	info_limit['servermod'] = 'scmccClient'
+	info_pass['servermod'] = 'scmccClient'
+elseif string.find(url,"scmccClientWap") then
+	info_limit['servermod'] = 'scmccClientWap'
+	info_pass['servermod'] = 'scmccClientWap'
+elseif string.find(url,"scmccCampaign") then
+	info_limit['servermod'] = 'scmccCampaign'
+	info_pass['servermod'] = 'scmccCampaign'
+else
+	info_limit['servermod'] = 'scmcc'
+	info_pass['servermod'] = 'scmcc'
+end
+
+local upgrade_switch,err = rediscmd:hget('upgrade','switch') -- on/off/nil
+local upgrade_proxy,err = upgrade:upgrade_proxy() --true/nil
+if upgrade_switch == 'off' or upgrade_switch == nil then
+
+	limit_mode:limit(info_pass,info_limit)
+elseif upgrade_switch == 'on' and upgrade_proxy then
+	-- ngx.say(info_pass.status)
+	upgrade:upgrade(upgrade_proxy,info_pass,info_limit)
+else
+	limit_mode:limit(info_pass,info_limit)
+end
