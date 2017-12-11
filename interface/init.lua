634fcc2a31413335fccc4590abbfa563b194afa2
delgyd <delgyd@163.com>
2017-12-11 17:50:06 +0800

    修改redis连接端口

--- interface/init.lua
+++ interface/init.lua
@@ -19,9 +19,9 @@ _M._VERSION = '0.0.1'
 
 _M.Redis = {
                 ['host']= '192.168.1.2',
-                ['port']=6388,
+                ['port']= 19000,
                 ['db_index']=0,
-                ['password']='qwert',
+                ['password']= 'qwert',
                 ['timeout']=1000,
                 ['keepalive']=60000,
                 ['pool_size']=100,
